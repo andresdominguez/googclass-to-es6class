59554e2c9b7decb90726c1b36fecd7d1af3c5231
Andres Dominguez <andres.dominguez.p@gmail.com>
2016-09-10 10:12:43 -0400

    Create files to encapsulate plugin runner and script that reads the cli args

--- run.js
+++ run.js
@@ -1,18 +1,2 @@
 #! /usr/bin/env node
-const fs = require('fs');
-const babel = require('babel-core');
-const googDefineClass = require('./goog-define-class');
-
-// read the filename from the command line arguments
-const fileName = process.argv[2];
-
-// read the code from this file
-const src = fs.readFileSync(fileName, 'utf8');
-
-// use our plugin to transform the source
-const out = babel.transform(src, {
-  plugins: [googDefineClass]
-});
-
-// print the generated code to screen
-console.log(out.code);
+require('./lib/transform_file');
