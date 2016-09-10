473e6f9c8c8656684f8a369a902c9c845a3a2407
Andres Dominguez <andres.dominguez.p@gmail.com>
2016-09-10 14:57:39 -0400

    implement super() call

--- test/inheritance_es6.js
+++ test/inheritance_es6.js
@@ -3,12 +3,15 @@ const Parent = goog.require('some.ns.MyParent');
 /**
  * This is foo bar.
  */
+
 class FooBar extends Parent {
   /**
    * Creates a new instance of...
    * @param {Object} name
    */
   constructor(name) {
+    super(name);
+
     this.name = name;
 
     /** @export {string} */
